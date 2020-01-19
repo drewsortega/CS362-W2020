ec4153b719febb597303832f1138fb7599bf60ba
Drew S. Ortega <DrewSOrtega@gmail.com>
2020-01-19 11:15:57 -0800

    fix line lengths for supply order

--- projects/ortegadr/dominion/testUtility.py
+++ projects/ortegadr/dominion/testUtility.py
@@ -30,9 +30,13 @@ get_boxes(nV):
     return box
 
 get_supply_order():
-    supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
+    supply_order = {0:['Curse','Copper'],
+                    2:['Estate','Cellar','Chapel','Moat'],
                     3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
-                    4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
-                    5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
-                    6:['Gold','Adventurer'],8:['Province']}
+                    4:['Gardens','Bureaucrat','Feast','Militia','Moneylender',
+                        'Remodel','Smithy','Spy','Thief','Throne Room'],
+                    5:['Duchy','Market','Council Room','Festival',
+                        'Laboratory','Library','Mine','Witch'],
+                    6:['Gold','Adventurer'],
+                    8:['Province']}
     return supply_order
