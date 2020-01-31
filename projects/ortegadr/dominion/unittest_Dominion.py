ba13061a1c826b4f48ac85539a2526303a1a772c
Drew S. Ortega <DrewSOrtega@gmail.com>
2020-01-30 21:33:15 -0800

    add test schematics for required unit tests, but all default to failing;

---
+++ projects/ortegadr/dominion/unittest_Dominion.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+import Dominion
+
+
+class TestAction_card(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_use(self):
+        self.fail()
+
+    def test_augment(self):
+        self.fail()
+
+
+class TestPlayer(TestCase):
+    def test_action_balance(self):
+        self.fail()
+
+    def test_calcpoints(self):
+        self.fail()
+
+    def test_draw(self):
+        self.fail()
+
+    def test_cardsummary(self):
+        self.fail()
+
+
+class TestGame(TestCase):
+    def test_gameover(self):
+        self.fail()
